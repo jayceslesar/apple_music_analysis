a8f80682a3c9a78a878627c91c9175bfc6907330
jayceslesar <47452474+jayceslesar@users.noreply.github.com>
2019-12-07 12:48:58 -0500

    made reclean as conservative as possible in favor of complete data

--- reclean.py
+++ reclean.py
@@ -1,14 +1,14 @@
 # recleaning with pandas in attempt to keep as most data as possible
 # with a focus on keeping complete data
 import pandas as pd
-import math
+import datetime
 
 
 # takes in a path to a dataframe
 # remove all rows that have a missing value in [Content Name, Artist Name, Event End Timestamp, Event Start Timestamp,]
 def active_clean(path):
     cols_to_check = ['Content Name', 'Artist Name',
-                     'Event Start Timestamp', 'Event End Timestamp']
+                     'Event Start Timestamp', 'Event End Timestamp', 'Client IP Address', 'Genre']
     rows_to_drop = []
     df = pd.read_csv(path, encoding='latin1')
     for col in cols_to_check:
@@ -18,9 +18,33 @@ def active_clean(path):
                 rows_to_drop.append(i)
     drop_these = list(set(rows_to_drop))
     df = df.drop(df.index[drop_these])
+    trash_cols = []
+    for col in df.columns:
+        if col not in cols_to_check:
+            trash_cols.append(col)
+    df = df.drop(columns=trash_cols)
+    song_artist = []
+    artists = df['Artist Name'].to_list()
+    songs = df['Content Name'].to_list()
+    for i in range(len(songs)):
+        song_artist.append(songs[i] + ' - ' + artists[i])
+    df['True Name'] = song_artist
+    years = []
+    months_num = []
+    months = []
+    weekdays = []
+    dates = df['Event Start Timestamp'].tolist()
+    for date in dates:
+        date_parts = date[:10].split('-')
+        years.append(date_parts[0])
+        months_num.append(date_parts[1])
+        curr_date = datetime.datetime(
+            int(date_parts[0]), int(date_parts[1]), int(date_parts[2]))
+        weekdays.append(curr_date.strftime('%A'))
+        months.append(curr_date.strftime('%b'))
+    df['Year'] = years
+    df['Month Num'] = months_num
+    df['Month Name'] = months
+    df['Weekday'] = weekdays
+    # add weather daa (IE sunny, rainy...)
     return df
-
-
-path = r'D:\apple_music_data\apple_music_data.csv'
-df = active_clean(path)
-df.to_csv('apple_super_clean.csv')
