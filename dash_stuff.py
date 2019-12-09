fd4a22e95309d5d234a23eb44c27da5d37ebc942
jayceslesar <47452474+jayceslesar@users.noreply.github.com>
2019-12-09 17:23:18 -0500

    formatting - will continue to perfect

--- dash_stuff.py
+++ dash_stuff.py
@@ -1,32 +1,52 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objs as go
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5],
-                    'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
+def dash_test(data):
+    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+    app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+    app.layout = html.Div(children=[
+        html.H1(children=''),
+        html.Div(children='''
+        '''),
+        dcc.Graph(
+            id='top_five_songs',
+            figure={
+                'data': data['graph'],
+                'layout': {
+                    'title': {
+                        'text': '<b>Top 5 Songs of 2019<b>',
+                        'y': '5',
+                        'x': '0.5',
+                        'xanchor': 'center'
+                    },
+                    'plot_bgcolor': '#ffffff',
+                    'paper_bgcolor': '#ffffff',
+                    'font': {
+                        'family': 'SF Pro',
+                        'color': '#000000',
+                        'size': '16'
+                    },
+                    'images': data['imgs'],
+                    'yaxis': {
+                        'title': {
+                            'text': '<b>Plays in 2019<b>',
+                            'size': '20'
+                        }
+                    },
+                    'height': '500',
+                    'margin': {
+                        'l': '150',
+                        'r': '50',
+                        'b': '100',
+                        't': '150',
+                        'pad': '4'
+                    }
+                }
             }
-        }
-    )
-])
+        )
+    ])
 
-if __name__ == '__main__':
     app.run_server(debug=True)
