0338a37f817b2ec319951742e44f59d889765878
jayceslesar <47452474+jayceslesar@users.noreply.github.com>
2020-01-03 13:54:47 -0500

    more weekday support

--- apple_data.py
+++ apple_data.py
@@ -25,51 +25,88 @@ class AppleData:
             return
 
     @staticmethod
+    def week_in_review_songs(self, month):
+        most_recent = self.data.iloc[-1]
+        most_recent_date = most_recent['Year'] + '-' + \
+            most_recent['Month Num'] + '-' + most_recent['Weekday Num']
+        dates = date_funcs.past_week_dates(most_recent_date)
+        better_dates = []
+        for date in dates:
+            curr = str(date).split('-')
+            better_dates.append([curr[0], curr[1], curr[2]])
+        better_dates = reversed(better_dates)
+        dfs = []
+        for date in better_dates:
+            df = self.data.loc[(self.data['Year'] == date[0])]
+            df = df.loc[(df['Month Num'] == date[1])]
+            df = df.loc[(df['Weekday Num'] == date[2])]
+            dfs.append(df)
+        df = pd.concat(dfs, axis=0)
+        NUM = 5
+        return_data = {}
+        return_data['top_five_songs'] = Counter(
+            df['True Name'].to_list()).most_common(NUM)
+        return_data['top_five_artists'] = Counter(
+            df['Artist Name'].to_list()).most_common(NUM)
+        return_data['top_five_genres'] = Counter(
+            df['Genre'].to_list()).most_common(NUM)
+        return_data['mins_total'] = format(date_funcs.mins_total(df), ',d')
+        return_data['plays_total'] = len(df)
+        return return_data
+
+    @staticmethod
     def month_in_review_songs(self, month):
+        NUM = 5
         return_data = {}
         year = max(self.valid_years)
         df = self.data.loc[(self.data['Year'] == year)]
         df = df.loc[(df['Month Name'] == month)]
         return_data['top_five_songs'] = Counter(
-            df['True Name'].to_list()).most_common(5)
+            df['True Name'].to_list()).most_common(NUM)
         return_data['top_five_artists'] = Counter(
-            df['Artist Name'].to_list()).most_common(5)
+            df['Artist Name'].to_list()).most_common(NUM)
         return_data['top_five_genres'] = Counter(
-            df['Genre'].to_list()).most_common(5)
+            df['Genre'].to_list()).most_common(NUM)
         return_data['mins_total'] = format(date_funcs.mins_total(df), ',d')
         return_data['plays_total'] = len(df)
         return return_data
 
     @staticmethod
     def year_in_review_songs(self, year):
+        NUM = 5
         return_data = {}
         df = self.data.loc[(self.data['Year'] == year)]
         return_data['top_five_songs'] = Counter(
-            df['True Name'].to_list()).most_common(5)
+            df['True Name'].to_list()).most_common(NUM)
         return_data['top_five_artists'] = Counter(
-            df['Artist Name'].to_list()).most_common(5)
+            df['Artist Name'].to_list()).most_common(NUM)
         return_data['top_five_genres'] = Counter(
-            df['Genre'].to_list()).most_common(5)
+            df['Genre'].to_list()).most_common(NUM)
         return_data['mins_total'] = format(date_funcs.mins_total(df), ',d')
         return_data['plays_total'] = len(df)
         return return_data
 
-    def top_five_songs_month_or_year_dash(self, month_or_year, song_or_artist):
-        if len(month_or_year) == 3:
+    def top_five_songs_month_or_year_dash(self, month_or_year_or_week, song_or_artist):
+        if len(month_or_year_or_week) == 3:
             type = 'month'
-        else:
+        if len(month_or_year_or_week) == 4:
             type = 'year'
+        if month_or_year_or_week == 'week':
+            type = 'week'
         GRAPH_HEIGHT = 0.945
         cols = ['song', 'artist', 'plays',
                 'album_art_rgb', 'image_link', 'song_artist']
         songs, artists, plays, album_art_rgb, image_link, song_artist = [
         ], [], [], [], [], []
         if type == 'year':
-            top_five = self.year_in_review_songs(self, month_or_year)[
-                'top_five_songs']
+            data = self.year_in_review_songs(self, month_or_year_or_week)
         if type == 'month':
-            top_five = self.month_in_review_songs(
-                self, month_or_year)['top_five_songs']
+            data = self.month_in_review_songs(
+                self, month_or_year_or_week)
+        if type == 'week':
+            data = self.week_in_review_songs(
+                self, month_or_year_or_week)
+        top_five = data['top_five_songs']
         i = 0
         for song in top_five:
             songs.append(song[0].split('-')[0][:-1])
@@ -79,7 +116,8 @@ class AppleData:
             search = search.encode('raw_unicode_escape').decode('utf-8')
             search = album_art.fix(search)
             plays.append(song[1])
-            album_art_rgb.append(album_art.compute_top_image_color(search))
+            album_art_rgb.append(
+                album_art.compute_top_image_color(search))
             image_link.append(album_art.get_art(search))
             song_artist.append(songs[i] + ' - ' + artists[i])
             i += 1
@@ -121,10 +159,11 @@ class AppleData:
         return_data['imgs'] = images
         return_data['type'] = type
         if type == 'month':
-            return_data['month'] = month_or_year
+            return_data['month'] = month_or_year_or_week
             return_data['year'] = max(self.valid_years)
         if type == 'year':
-            return_data['year'] = month_or_year
+            return_data['year'] = month_or_year_or_week
+        return_data['mins_total'] = data['mins_total']
         return return_data
 
     @staticmethod
